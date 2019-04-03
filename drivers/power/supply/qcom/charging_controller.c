8a5e31479afc9b741a1f6279a36fc19f9eb133e3
PrimoDev23 <lexx.ps2711@gmail.com>
2019-04-03 15:18:29 +0200

    charging_controller: add license header

--- drivers/power/supply/qcom/charging_controller.c
+++ drivers/power/supply/qcom/charging_controller.c
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2019, PrimoDev23 <lexx.ps2711@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
 #define module_name "charging_controller"
 
 #include <linux/moduleparam.h>
