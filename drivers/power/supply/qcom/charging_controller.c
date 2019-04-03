2ded9530b7691ad7b9ee7dd289e14b30b937d9bb
PrimoDev23 <lexx.ps2711@gmail.com>
2019-04-03 15:09:14 +0200

    Introduce Charging Controller with night mode (v1)
    This is another charging module made by me.
    It calculates the needed charging rate by the capacity needed to charge
    till 100% or whatever is set for stop charge value of charging limiter
    (for now with static voltages -> they
    are left out bc the will not be needed in the calculation)
    
    There will be more updates in future.
    Now there is only one bug... After setting low charging rates and turning off
    night mode, the charging rate will stay low
    This is easily fixable with a reboot, but anyways it's an annoying issue
    and will be fixed in some of the next builds.

--- drivers/power/supply/qcom/charging_controller.c
+++ drivers/power/supply/qcom/charging_controller.c
@@ -5,35 +5,45 @@
 #include "charging_controller.h"
 
 static unsigned int input_current_limit = 0; //0 = disabled module
-static unsigned int mode = 0; //0 = off, 1 = night mode, 2 = fast charge (just increased maximum current for now)
+static unsigned int mode = 0; //0 = off, 1 = night mode, 2 = fast charge (not there yet)
+static unsigned int time_h = 6;
 
 module_param(input_current_limit, uint, 0644);
 module_param(mode, uint, 0644);
+module_param(time_h, uint, 0644);
 
-extern int calculate_max_current(unsigned int cap_battery_now)
+//Gets called from outside and selects by mode
+extern int calculate_max_current(unsigned int cap_battery_now, unsigned int charge_till_cap)
 {
-	if(mode == 0)
+
+	pr_info("%s: %i", module_name, mode);
+
+	//Fallback if capacity is above 100 (seems not to happen often but can)
+	if(cap_battery_now > 100 || charge_till_cap > 100)
 		return 0;
 
+	//Mode 1 = Night mode
 	if(mode == 1){
-		return night_mode_icl(cap_battery_now);
+		pr_info("%s: calculating with night mode", module_name);
+		return night_mode_icl(cap_battery_now, charge_till_cap);
 	}
 
 	return 0;
 }
 
-extern int night_mode_icl(unsigned int cap_battery_now)
+//Calculates night mode icl
+extern int night_mode_icl(unsigned int cap_battery_now, unsigned int charge_till_cap)
 {
-	//init voltages for battery capacities and calc average
-	unsigned int thirty_voltage = 3700; //mv
-	unsigned int eighty_voltage = 4000; //mv
-	float average = (thirty_voltage + eighty_voltage) / 2; //average will be used to calculate needed charging
-	
-	unsigned int capacity = 2000 - (4000 * (cap_battery_now - 30) / 100); //Calculate left capacity to 80%
-	float power_batt_valued = capacity * average;
-	unsigned int time_h = 6; //expose to userspace later
-
-	unsigned int icl = (power_batt_valued / time_h * average) * 1000;
+	//Don't let the user set values below 4 and above 8 for time in hours
+	if (time_h > 6)
+		time_h = 6;
+	else if (time_h < 4)
+		time_h = 4;
+	int needed_capacity = 4000 * (charge_till_cap - cap_battery_now) / 100; //Calculate left capacity to 80%
+	pr_info("%s: needed_capacity = %i", module_name, needed_capacity);
+	//Calculate icl, print and return it
+	//Since I am using average values i can leave out voltages completely (Eventually will reintroduce them later)
+	int icl = (needed_capacity / time_h) * 1000;
 
 	pr_info("%s: %i", module_name, icl);
 	return icl;
--- drivers/power/supply/qcom/charging_controller.h
+++ drivers/power/supply/qcom/charging_controller.h
@@ -3,8 +3,8 @@
 
 #include <linux/types.h>
 
-extern int calculate_max_current(unsigned int cap_battery_now);
-extern int night_mode_icl(unsigned int cap_battery_now);
+extern int calculate_max_current(unsigned int cap_battery_now, unsigned int charge_till_cap);
+extern int night_mode_icl(unsigned int cap_battery_now, unsigned int charge_till_cap);
 
 
 #endif /* _CHARGING_CONTROLLER_H_ */
