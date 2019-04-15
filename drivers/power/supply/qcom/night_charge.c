a60fbf6526145c867cf0e8286a7e4d5996288df4
PrimoDev23 <lexx.ps2711@gmail.com>
2019-04-15 11:19:57 +0200

    night_charge: Rework calculation to recalculate every 2 hours
    This will lead to better and more exact results

--- drivers/power/supply/qcom/night_charge.c
+++ drivers/power/supply/qcom/night_charge.c
@@ -15,32 +15,51 @@
  * v2 - Correct calculation and fix issue that reboot needed to get back normal charging 
  */
 
-#define module_name "charging_controller"
+#define module_name "night_charge"
 
 #include <linux/moduleparam.h>
-#include <linux/state_notifier.h>
-#include "charging_controller.h"
+#include <linux/time.h>
+#include "night_charge.h"
 
-static unsigned int mode = 0; //0 = off, 1 = night mode, 2 = fast charge (not there yet)
+static unsigned bool active = true; //0 = off, 1 = night mode, 2 = fast charge (not there yet)
 static unsigned int time_h = 6;
 static unsigned int charger_voltage = 5; //Use this to calculate charging time
+static int last_check_s = 0;
+static int left_time_s = 0;
 
-module_param(mode, uint, 0644);
+module_param(active, bool, 0644);
 module_param(time_h, uint, 0644);
 module_param(charger_voltage, uint, 0644);
 
 //Gets called from outside and selects by mode
 extern int calculate_max_current(unsigned int cap_battery_now, unsigned int charge_till_cap)
 {
-
-	pr_info("%s: %i", module_name, mode);
-
-	//Fallback if capacity is above 100 (seems not to happen often but can)
-	if(cap_battery_now > 100 || charge_till_cap > 100)
+	//Fallback if capacity is >= 100
+	if(cap_battery_now >= 100 || charge_till_cap >= 100)
 		return 0;
 
-	//Mode 1 = Night mode
-	if(mode == 1){
+	if(active){
+		//Don't let the user set values below 4 and above 8 for time in hours
+	        if (time_h > 6)
+        	        time_h = 6;
+	        else if (time_h < 4)
+        	        time_h = 4;
+		struct timespec ts;
+		getnstimeofday(*ts);
+		//Started charging
+		if(left_time_s == 0){
+			left_time_s = time_h * 3600;
+		}
+		//Fallback for crossing midnight
+		if((last_check_s - 86400 + 7200) > 0 && (last_check_s - 86400 + 7200) < ts.tv_sec){
+			left_time_s = left_time_s - (86400 - last_check_s + ts.tv_sec);
+		}
+		else if(last_check_s + 7200 < ts.tv_sec) //If not crossing midnight just calculate left time
+		{
+			left_time_ms = left_time_s - (ts.tv_sec - last_check_s);
+		}
+		else //If not 2h over don't recalculate
+			return 0;
 		pr_info("%s: calculating with night mode", module_name);
 		return night_mode_icl(cap_battery_now, charge_till_cap);
 	}
@@ -51,27 +70,29 @@ extern int calculate_max_current(unsigned int cap_battery_now, unsigned int char
 //Calculates night mode icl
 extern int night_mode_icl(unsigned int cap_battery_now, unsigned int charge_till_cap)
 {
-	int needed_capacity, icl;
-	//Don't let the user set values below 4 and above 8 for time in hours
-	if (time_h > 6)
-		time_h = 6;
-	else if (time_h < 4)
-		time_h = 4;
+	int needed_capacity, icl, left_time_h;
+	left_time_h = left_time_s / 3600;
 	needed_capacity = 4000 * (charge_till_cap - cap_battery_now) / 100; //Calculate left capacity to 80%
 	pr_info("%s: needed_capacity = %i", module_name, needed_capacity);
 	pr_info("%s: charger_voltage = %i", module_name, charger_voltage);
-	pr_info("%s: time_h = %i", module_name, time_h);
+	pr_info("%s: left_time_h = %i", module_name, left_time_h);
 	//Calculate icl, print and return it
 	//Since I am using average values i can leave out voltages completely (Eventually will reintroduce them later)
-	icl = ( (needed_capacity * 405 / 100) / (time_h * charger_voltage) ) * 1000;
+	icl = ( (needed_capacity * 405 / 100) / (left_time_h * charger_voltage) ) * 1000;
 	pr_info("%s: %i", module_name, icl);
 	return icl;
 }
 
-static int __init init_charging_controller()
+//Reset values on unplug charger
+extern void reset_values(){
+	left_time_s = 0;
+	last_check_s = 0;
+}
+
+static int __init init_night_charge()
 {
 	//Just print init message for now
 	pr_info("%s: Initialized!", module_name);
 	return 0;
 }
-late_initcall(init_charging_controller);
+late_initcall(init_night_charge);
--- drivers/power/supply/qcom/night_charge.h
+++ drivers/power/supply/qcom/night_charge.h
@@ -1,10 +1,10 @@
-#ifndef _CHARGING_CONTROLLER_H_
-#define _CHARGING_CONTROLLER_H_
+#ifndef _NIGHT_CHARGE_H_
+#define _NIGHT_CHARGE_H_
 
 #include <linux/types.h>
 
 extern int calculate_max_current(unsigned int cap_battery_now, unsigned int charge_till_cap);
 extern int night_mode_icl(unsigned int cap_battery_now, unsigned int charge_till_cap);
+extern void reset_values();
 
-
-#endif /* _CHARGING_CONTROLLER_H_ */
+#endif /* _NIGHT_CHARGE_H_ */
