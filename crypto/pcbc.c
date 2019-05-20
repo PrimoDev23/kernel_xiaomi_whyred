2535faf937a938ff6bae64f8042b68c41dab3423
srfarias <erikofarias@gmail.com>
2019-05-20 12:28:18 -0300

    crypto: avoid VLA
    Signed-off-by: srfarias <erikofarias@gmail.com>

--- crypto/pcbc.c
+++ crypto/pcbc.c
@@ -77,7 +77,7 @@ static int crypto_pcbc_encrypt_inplace(struct blkcipher_desc *desc,
 	unsigned int nbytes = walk->nbytes;
 	u8 *src = walk->src.virt.addr;
 	u8 *iv = walk->iv;
-	u8 tmpbuf[bsize];
+	u8 tmpbuf[MAX_CIPHER_BLOCKSIZE];
 
 	do {
 		memcpy(tmpbuf, src, bsize);
@@ -157,7 +157,7 @@ static int crypto_pcbc_decrypt_inplace(struct blkcipher_desc *desc,
 	unsigned int nbytes = walk->nbytes;
 	u8 *src = walk->src.virt.addr;
 	u8 *iv = walk->iv;
-	u8 tmpbuf[bsize];
+	u8 tmpbuf[MAX_CIPHER_BLOCKSIZE];
 
 	do {
 		memcpy(tmpbuf, src, bsize);
