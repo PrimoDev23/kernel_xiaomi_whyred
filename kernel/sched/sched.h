cedad81629692064c1cfc73d0c37fc590fdcc916
PrimoDev23 <lexx.ps2711@gmail.com>
2019-07-11 17:20:25 +0200

    Revert "sched: energy: calculate and update CPU capacity dynamically"
    we need to revert this for using capacity based EM
    
    This reverts commit 87a63d7abfc52ee7363456081f39b03681ce1d5f.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1591,13 +1591,6 @@ unsigned long arch_scale_freq_capacity(struct sched_domain *sd, int cpu)
 }
 #endif
 
-#ifndef arch_update_cpu_capacity
-static __always_inline
-void arch_update_cpu_capacity(int cpu)
-{
-}
-#endif
-
 #ifdef CONFIG_SMP
 static inline unsigned long capacity_of(int cpu)
 {
