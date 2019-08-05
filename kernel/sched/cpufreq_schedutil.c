1445ca358fc6075745ea2cce2cea03988cff097c
Danny Lin <danny@kdrag0n.dev>
2019-08-05 06:45:05 +0000

    cpufreq: schedutil: Expose default rate-limits as config options
    This allows us to tune the default rate-limits without constantly
    changing schedutil's code.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -797,8 +797,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = LATENCY_MULTIPLIER;
-	tunables->down_rate_limit_us = LATENCY_MULTIPLIER;
+	tunables->up_rate_limit_us = CONFIG_SCHEDUTIL_UP_RATE_LIMIT;
+	tunables->down_rate_limit_us = CONFIG_SCHEDUTIL_DOWN_RATE_LIMIT;
 	lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 	if (lat) {
 		tunables->up_rate_limit_us *= lat;
